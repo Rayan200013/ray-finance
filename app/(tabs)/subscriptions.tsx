b9bde2bbf2ad932666688ecab6354c5f9d2eb483
Rayan200013 <rayan.bouezzeddine13@gmail.com>
2026-04-13 07:40:39 +0300

    Project setup, routing and navigations

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,10 +1,13 @@
 import { Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {styled} from 'nativewind';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+ 
+const SafeAreaView = styled(RNSafeAreaView)
 
 const Subscriptions = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='flex-1 p-5'>
       <Text>Subscriptions</Text>
     </SafeAreaView>
   )
